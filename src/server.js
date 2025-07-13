07958220b9a121cfd6b2637e56be146025b6100e
Tahsin566 <nazmulhassantahsin566@gmail.com>
2025-07-13 20:06:56 +0600

    server

--- src/server.js
+++ src/server.js
@@ -36,7 +36,7 @@ app.use('/css',express.static('views/css'))
 
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://ieeelusb.netlify.app',
     credentials:true
 }))
 app.use(cookieParser())
